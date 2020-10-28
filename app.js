ef1fe481cc44ea60759df1e8c4f16be75ff72662
muktaking <yeakubinfire@gmail.com>
2020-10-29 02:18:57 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const app = require("express")();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const contact = require("./contact");
+const port = process.env.PORT | 3000;
+// parse body params and attache them to req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors());
+
+// mount api v1 routes
+app.use("/contact", contact);
+app.use("/", (req, res) => {
+  res.send("OK");
+});
+
+app.listen(port, () => console.log(`server started on port ${port}`));
