6c61f25795612944c9af3b834444615ef6579747
muktaking <yeakubinfire@gmail.com>
2020-10-29 02:36:59 +0600

    contact edited

--- contact.js
+++ contact.js
@@ -4,14 +4,17 @@ const router = express.Router();
 
 module.exports = router.post("/", (req, res) => {
   const { name, email, phone, message } = req.body.data;
-  var logStream = fs.createWriteStream("data.csv", { flags: "a" });
-  // use {flags: 'a'} to append and {flags: 'w'} to erase and write a new file
-  logStream.write(
-    `${name.replace(/\,/g, "*#*")},${email.replace(
-      /\,/g,
-      "*#*"
-    )},${phone.replace(/\,/g, "*#*")},${message.replace(/\,/g, "*#*")}`
-  );
-  logStream.end("\n");
-  res.json(req.body.data);
+  if (name.length > 0 && email.length > 0 && message.length > 0) {
+    var logStream = fs.createWriteStream("data.csv", { flags: "a" });
+    // use {flags: 'a'} to append and {flags: 'w'} to erase and write a new file
+    logStream.write(
+      `${name.replace(/\,/g, "*#*")},${email.replace(
+        /\,/g,
+        "*#*"
+      )},${phone.replace(/\,/g, "*#*")},${message.replace(/\,/g, "*#*")}`
+    );
+    logStream.end("\n");
+    res.json(req.body.data);
+  }
+  res.status(404);
 });
